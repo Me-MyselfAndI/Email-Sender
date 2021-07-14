f04d1b7f32bcaf6099528ae69729bed169a5b4e1
iBoyGrisha() <32421843+Me-MyselfAndI@users.noreply.github.com>
2021-07-14 13:06:38 -0400

    - Added support for custom text messages  - Fixed bug that prevented sending messages to non-TMobile numbers

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import smtplib, random
+import smtplib, random, threading
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.message import EmailMessage
 from kivy.app import App
+from kivy.uix.popup import Popup
+from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.pagelayout import PageLayout
+from time import sleep
 
 #Global Variables
 sender_email = "slabysh2015@gmail.com"
@@ -31,6 +34,8 @@ class GUILayout(PageLayout):
         self.send_button = ObjectProperty(None)
         self.emails = ObjectProperty(None)
         self.names = ObjectProperty(None)
+
+        self.__button_needs_update = False
         super(GUILayout, self).__init__(**kwargs)
         Window.size = (1200, 760)
 
@@ -39,7 +44,7 @@ class GUILayout(PageLayout):
             self.comm_mode = type
             self.mode_label.text = "Recipients' " + type[0].upper() + type[1:] + "s"
         else:
-            print("\u001b[34mALERT!!! Communication means st incorrectly!")
+            print("\u001b[34mALERT!!! Communication means set incorrectly!")
             self.comm_mode = None
 
     def transfer_input (self):
@@ -54,11 +59,35 @@ class GUILayout(PageLayout):
             elif raw_lines[i] != '\n':
                 print("\u001b[35mERROR!! INCORRECT INPUT FILE!!")
                 quit(0)
+
         recipients_file.close()
 
     def submit(self):
         raw_emails = self.emails.text.split("\n")
         raw_names = self.names.text.split("\n")
+
+        for i in range(len(raw_emails)):
+            if (raw_emails[i] == ""):
+                continue
+            if self.comm_mode == "email":
+                if not ("@" in raw_emails[i] and "." in raw_emails[i]):
+                    print("Wrong email:", raw_emails[i])
+                    popup = Popup(title="Wrong email: " + raw_emails[i], content=Button(text='I will check!', size_hint=(1, None), pos_hint={'center': 0.5}, height=30),
+                                  auto_dismiss=False, size_hint=(None, None), size=(400, 100))
+                    popup.content.bind(on_press=popup.dismiss)
+                    popup.open()
+                    return
+            else:
+                raw_emails[i] = raw_emails[i].replace('-', '').replace('.', '').replace("(", '').replace(")", "")
+                if "@" in raw_emails[i] or not all(x.isnumeric() or x == '+' for x in raw_emails[i]):
+                    print("Did you mean to send phone messages?")
+                    popup = Popup(title="Wrong phone number: " + raw_emails[i],
+                                  content=Button(text='I will check!', size_hint=(1, None), pos_hint={'center': 0.5},
+                                                 height=30),
+                                  auto_dismiss=False, size_hint=(None, None), size=(400, 100))
+                    popup.content.bind(on_press=popup.dismiss)
+                    popup.open()
+                    return
 
         recipients_str = ""
         for email, name in zip (raw_emails, raw_names):
@@ -69,37 +98,16 @@ class GUILayout(PageLayout):
         recipients_file.write (recipients_str)
         recipients_file.close()
 
-        '''
-        print("""
-                        Send to:
-                        1: EMAIL
-                        2: PHONE NUMBER
-                    """)
-        self.comm_mode = get_integer("Enter a number: ")
-        '''
 
         if self.comm_mode == "email":
 
             self.transfer_input()
 
-            print("\n\nThis is the list of parents' emails and names: ")
+            print("\n\nThis is the list of clients' emails and names: ")
             for i in range(len(self.parents_contacts)):
                 print(str(i + 1) + ":\tEmail:", self.parents_contacts[i], "\n\tName:", self.parents_names[i])
 
             password = input("\n\u001b[33mTo confirm the list, enter the password for your email:\n\u001b[0m")
-
-            fp = open("theCoderSchool - Progress Tracking.jpg", 'rb')
-            image1 = fp.read()
-            fp.close()
-
-            fp = open("coder tree.jpg", 'rb')
-            image2 = fp.read()
-            fp.close()
-
-            fp = open("signature.png", 'rb')
-            signature = fp.read()
-            fp.close()
-
             served_recipients = open("old_recipients.txt", 'a')
 
             for receiver_email, name in zip(self.parents_contacts, self.parents_names):
@@ -111,7 +119,7 @@ class GUILayout(PageLayout):
                     server.login(sender_email, password)
                     print("Login success")
 
-                    fp = open("email_body.html") if random.randrange(0, 1) > 0.5 else open("email_body_alt.html")
+                    fp = open("email_body.html")
                     email_body = fp.read(),
                     email_body = email_body[0]
                     fp.close()
@@ -141,10 +149,32 @@ class GUILayout(PageLayout):
         elif self.comm_mode == "phone":
             self.transfer_input()
 
-            for phone_num in self.parents_contacts:
-                number_alert('Hello Bitch', phone_num)
-                print("Success")
+            for phone_num, name in zip(self.parents_contacts, self.parents_names):
+                    print("Sending the email")
+                    try:
+                        print("\n\nThis is the list of clients' phone numbers and names: ")
+                        for i in range(len(self.parents_contacts)):
+                            print(str(i + 1) + ":\tEmail:", self.parents_contacts[i], "\n\tName:",
+                                  self.parents_names[i])
 
+                        password = input(
+                            "\n\u001b[33mTo confirm the list, enter the password for your email:\n\u001b[0m")
+
+                        fp = open("email_body.html")
+                        email_body = fp.read(),
+                        email_body = email_body[0]
+                        fp.close()
+                        email_body = email_body.format(name=name)
+
+                        email_msg = EmailMessage()
+                        email_msg.set_content(email_body)
+
+                        email_msg['Subject'] = subject
+                        email_msg['From'] = sender_email
+
+                        number_alert(email_msg, phone_num)
+                    except Exception as exception:
+                        print("\u001b[34m\tException happened:\n" + str(exception))
 
 
 class GUI(App):
@@ -161,26 +191,23 @@ def get_integer(prompt):
             return int(num)
         print("Invalid input please enter a number")
 
-def number_alert(body, to):
-    global sender_email, subject, password
-    provider = ["@tmomail.net", "@mms.att.net", "@mms.uscc.net", "@vzwpix.com"]
+def number_alert(email_msg, to):
+    provider = ["@vzwpix.com", "@tmomail.net", "@mms.att.net", "@mms.uscc.net"]
     to = str(to)#Convert digital number into text
     i = 0
-    while i < len(provider):
-        selected = provider[i]
-        i += 1
-        receiver = to + selected
-        msg = EmailMessage()
-        msg.set_content(body)
-        msg['subject'] = subject
-        msg['to'] = receiver
-        msg['from'] = sender_email
-
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(sender_email, password)
-        server.send_message(msg)
-        server.quit()
+    for selected in provider:
+        try:
+            receiver = to + selected
+            email_msg["To"] = receiver
+            print(email_msg["To"])
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(sender_email, password)
+            server.send_message(email_msg)
+            server.quit()
+            return
+        except Exception:
+            print("Wrong operator:", selected, "- trying further")
 
 if __name__ == '__main__':
     GUI().run()
