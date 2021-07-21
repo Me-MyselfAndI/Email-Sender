a653bf1a2e9a5df1f682b0ab9814e1ce2d1be868
iBoyGrisha() <32421843+Me-MyselfAndI@users.noreply.github.com>
2021-07-20 20:50:43 -0400

    - Made the fields-template appear dynamically

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.message import EmailMessage
 from kivy.app import App
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import Rectangle
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
+from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.pagelayout import PageLayout
+from kivy.uix.boxlayout import BoxLayout
 from time import sleep
 
 #Global Variables
@@ -22,21 +28,83 @@ password = 'vkoxtfzsofcjmrig'
 recipient = '7707574196@tmomail.net'
 recipients_file_name = "recipients.txt"
 
+class PromptAddNew(BoxLayout):
+    orientation = "horizontal"
+    columns = 2
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.button_add_short = Button(text="Add short textbox", size_hint=(0.5, None), height=40)
+        self.button_add_long = Button(text="Add short textbox", size_hint=(0.5, None), height=40)
+        self.add_widget(self.button_add_short)
+        self.add_widget(self.button_add_long)
+
+    def on_parent(self, screen, parent):
+        self.button_add_short.on_press = self.add_short_field
+        self.button_add_long.on_press = self.add_long_field
+
+    def add_short_field(self):
+        self.add_field(is_long=False)
+
+    def add_long_field(self):
+        self.add_field(is_long=True)
+
+    def add_field(self, is_long=False):
+        self.parent.add_widget(TextBoxSetup(is_long=is_long))
+
+class TextBoxSetup(GridLayout):
+    class DeleteButton(Button):
+        def __init__(self, text, **kwargs):
+            super().__init__(**kwargs)
+            self.text = " Delete " + text
+            self.size_hint = (None, None)
+            self.pos_hint = {"top": 1}
+            self.height = 60
+            self.width = 60
+
+    class FieldTitle (Label):
+        def __init__(self, text, is_long=False, **kwargs):
+            super().__init__(**kwargs)
+            self.text = text + " " + ("(short)" if not is_long else "(long)")
+            self.background_color = (0.3, 0.3, 0.3, 1)
+            self.size_hint = (1, None)
+            self.height = 30
+
+        def on_size(self, screen, parent):
+            self.canvas.before.clear()
+            with self.canvas.before:
+                Color(rgba=self.background_color)
+                Rectangle(size=self.size, pos=self.pos)
+
+    def __init__(self, name="", is_long=False, **kwargs):
+        super().__init__(**kwargs)
+        self.delete_button = self.DeleteButton (name, **kwargs)
+        self.field_title = self.FieldTitle (name, is_long=is_long, **kwargs)
+        self.setup_label_field = TextInput (size_hint=(1, None), height=30, text="Type the title of this field here")
+        self.cols = 4
+        self.spacing = 10
+        self.size_hint = (0.22, None)
+        self.all_widgets = BoxLayout()
+        self.all_widgets.add_widget(self.delete_button)
+        self.grid_layout = GridLayout(size_hint=(0.8, 1), cols=1)
+        self.grid_layout.add_widget(self.field_title)
+        self.grid_layout.add_widget(self.setup_label_field)
+        self.all_widgets.add_widget(self.grid_layout)
+        self.add_widget(self.all_widgets)
 
 
 Builder.load_file("design.kv")
 class GUILayout(PageLayout):
     def __init__(self, **kwargs):
+        super(GUILayout, self).__init__(**kwargs)
         self.parents_contacts = []
         self.parents_names = []
         self.comm_mode = "email"
+        self.setup_page = ObjectProperty(None)
         self.mode_label = ObjectProperty(None)
         self.send_button = ObjectProperty(None)
-        self.emails = ObjectProperty(None)
+        self.contacts = ObjectProperty(None)
         self.names = ObjectProperty(None)
 
-        self.__button_needs_update = False
-        super(GUILayout, self).__init__(**kwargs)
         Window.size = (1200, 760)
 
     def set_comm_mode(self, type):
@@ -46,7 +114,6 @@ class GUILayout(PageLayout):
         else:
             print("\u001b[34mALERT!!! Communication means set incorrectly!")
             self.comm_mode = None
-
 
     def transfer_input (self):
         recipients_file = open("recipients.txt", 'r')
@@ -64,25 +131,25 @@ class GUILayout(PageLayout):
         recipients_file.close()
 
     def submit(self):
-        raw_emails = self.emails.text.split("\n")
+        raw_contacts = self.contacts.text.split("\n")
         raw_names = self.names.text.split("\n")
 
-        for i in range(len(raw_emails)):
-            if (raw_emails[i] == ""):
+        for i in range(len(raw_contacts)):
+            if (raw_contacts[i] == ""):
                 continue
             if self.comm_mode == "email":
-                if not ("@" in raw_emails[i] and "." in raw_emails[i]):
-                    print("Wrong email:", raw_emails[i])
-                    popup = Popup(title="Wrong email: " + raw_emails[i], content=Button(text='I will check!', size_hint=(1, None), pos_hint={'center': 0.5}, height=30),
+                if not ("@" in raw_contacts[i] and "." in raw_contacts[i]):
+                    print("Wrong email:", raw_contacts[i])
+                    popup = Popup(title="Wrong email: " + raw_contacts[i], content=Button(text='I will check!', size_hint=(1, None), pos_hint={'center': 0.5}, height=30),
                                   auto_dismiss=False, size_hint=(None, None), size=(400, 100))
                     popup.content.bind(on_press=popup.dismiss)
                     popup.open()
                     return
             else:
-                raw_emails[i] = raw_emails[i].replace('-', '').replace('.', '').replace("(", '').replace(")", "")
-                if "@" in raw_emails[i] or not all(x.isnumeric() or x == '+' for x in raw_emails[i]):
+                raw_contacts[i] = raw_contacts[i].replace('-', '').replace('.', '').replace("(", '').replace(")", "")
+                if "@" in raw_contacts[i] or not all(x.isnumeric() or x == '+' for x in raw_contacts[i]):
                     print("Did you mean to send phone messages?")
-                    popup = Popup(title="Wrong phone number: " + raw_emails[i],
+                    popup = Popup(title="Wrong phone number: " + raw_contacts[i],
                                   content=Button(text='I will check!', size_hint=(1, None), pos_hint={'center': 0.5},
                                                  height=30),
                                   auto_dismiss=False, size_hint=(None, None), size=(400, 100))
@@ -91,8 +158,8 @@ class GUILayout(PageLayout):
                     return
 
         recipients_str = ""
-        for email, name in zip (raw_emails, raw_names):
-            recipients_str += (email + '\n' + name + "\n\n")
+        for contact, name in zip (raw_contacts, raw_names):
+            recipients_str += (contact + '\n' + name + "\n\n")
         recipients_str = recipients_str[:len(recipients_str)-1]
 
         recipients_file = open(recipients_file_name, 'w')
@@ -104,7 +171,7 @@ class GUILayout(PageLayout):
 
             self.transfer_input()
 
-            print("\n\nThis is the list of clients' emails and names: ")
+            print("\n\nClients' emails and names: ")
             for i in range(len(self.parents_contacts)):
                 print(str(i + 1) + ":\tEmail:", self.parents_contacts[i], "\n\tName:", self.parents_names[i])
 
@@ -148,9 +215,9 @@ class GUILayout(PageLayout):
         elif self.comm_mode == "phone":
             self.transfer_input()
 
-            print("\n\nThis is the list of clients' phone numbers and names: ")
+            print("\n\nClients' phone numbers and names: ")
             for i in range(len(self.parents_contacts)):
-                print(str(i + 1) + ":\tEmail:", self.parents_contacts[i], "\n\tName:",
+                print(str(i + 1) + ":\tPhone #:", self.parents_contacts[i], "\n\tName:",
                       self.parents_names[i])
             password = input("\n\u001b[33mTo confirm the list, enter the password for your email:\n\u001b[0m")
             for phone_num, name in zip(self.parents_contacts, self.parents_names):
