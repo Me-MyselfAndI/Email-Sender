97a439ebd0c137cb10aa5f8e0acfc61b0eff3328
iBoyGrisha() <32421843+Me-MyselfAndI@users.noreply.github.com>
2021-07-20 21:26:17 -0400

    - Fixed some bugs in the dynamically adding fields

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class PromptAddNew(BoxLayout):
         self.add_widget(self.button_add_short)
         self.add_widget(self.button_add_long)
 
-    def on_parent(self, screen, parent):
+    def on_parent(self, *args):
         self.button_add_short.on_press = self.add_short_field
         self.button_add_long.on_press = self.add_long_field
 
@@ -53,9 +53,9 @@ class PromptAddNew(BoxLayout):
 
 class TextBoxSetup(GridLayout):
     class DeleteButton(Button):
-        def __init__(self, text, **kwargs):
+        def __init__(self, **kwargs):
             super().__init__(**kwargs)
-            self.text = " Delete " + text
+            self.text = " Delete "
             self.size_hint = (None, None)
             self.pos_hint = {"top": 1}
             self.height = 60
@@ -64,22 +64,33 @@ class TextBoxSetup(GridLayout):
     class FieldTitle (Label):
         def __init__(self, text, is_long=False, **kwargs):
             super().__init__(**kwargs)
-            self.text = text + " " + ("(short)" if not is_long else "(long)")
+            self.is_long = is_long
+            self.text = "ENTER TITLE " + ("(short)" if not is_long else "(long)")
             self.background_color = (0.3, 0.3, 0.3, 1)
             self.size_hint = (1, None)
             self.height = 30
-
-        def on_size(self, screen, parent):
-            self.canvas.before.clear()
+            self.bind (pos=self.update, size=self.update)
             with self.canvas.before:
                 Color(rgba=self.background_color)
-                Rectangle(size=self.size, pos=self.pos)
+                self.rect = Rectangle(size=self.size, pos=self.pos)
+
+        def update(self, *args):
+            self.rect.pos = self.pos
+            self.rect.size = self.size
+
+        def on_text (self, *args):
+            try:
+                self.parent.text = self.text
+            except Exception:
+                pass
 
     def __init__(self, name="", is_long=False, **kwargs):
         super().__init__(**kwargs)
-        self.delete_button = self.DeleteButton (name, **kwargs)
+        self.text = name
+        self.delete_button = self.DeleteButton (**kwargs)
         self.field_title = self.FieldTitle (name, is_long=is_long, **kwargs)
-        self.setup_label_field = TextInput (size_hint=(1, None), height=30, text="Type the title of this field here")
+        self.setup_label_field = TextInput (size_hint=(1, None), height=30, text="", hint_text="This field's title")
+        self.setup_label_field.bind(text=self.update)
         self.cols = 4
         self.spacing = 10
         self.size_hint = (0.22, None)
@@ -90,6 +101,11 @@ class TextBoxSetup(GridLayout):
         self.grid_layout.add_widget(self.setup_label_field)
         self.all_widgets.add_widget(self.grid_layout)
         self.add_widget(self.all_widgets)
+
+    def update (self, *args):
+        self.text = self.setup_label_field.text
+        self.field_title.text = self.text
+        self.field_title.text = self.text + " " + ("(short)" if not self.field_title.is_long else "(long)")
 
 
 Builder.load_file("design.kv")
